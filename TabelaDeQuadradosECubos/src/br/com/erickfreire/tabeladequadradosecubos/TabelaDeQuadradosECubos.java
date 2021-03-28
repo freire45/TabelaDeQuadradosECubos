f196718aade030eb8fb171d7a5d9e82a3d2e1047
Erick Freire <erilefr@gmail.com>
2021-03-27 23:41:45 -0300

    TabelaDeQuadradosECubos
    TabelaDeQuadradosECubos

---
+++ TabelaDeQuadradosECubos/src/br/com/erickfreire/tabeladequadradosecubos/TabelaDeQuadradosECubos.java
@@ -0,0 +1,29 @@
+package br.com.erickfreire.tabeladequadradosecubos;
+
+/**
+ * Crie um prorgama em Java que mostre uma tabela com o quadrado e o cubo dos valores de 0 a 10
+ * @author Erick Freire
+ * @version 1 - Criado em 27-03-2021 as 23:31
+ *
+ */
+
+public class TabelaDeQuadradosECubos {
+	public static void main(String[] args) {
+		
+		System.out.print("Numero	Quadrado	Cubo\n");
+		System.out.printf("%d		%d		%d%n", 0, 0*0, 0*0*0);
+		System.out.printf("%d		%d		%d%n", 1, 1*1, 1*1*1);
+		System.out.printf("%d		%d		%d%n", 2, 2*2, 2*2*2);
+		System.out.printf("%d		%d		%d%n", 3, 3*3, 3*3*3);
+		System.out.printf("%d		%d		%d%n", 4, 4*4, 4*4*4);
+		System.out.printf("%d		%d		%d%n", 5, 5*5, 5*5*5);
+		System.out.printf("%d		%d		%d%n", 6, 6*6, 6*6*6);
+		System.out.printf("%d		%d		%d%n", 7, 7*7, 7*7*7);
+		System.out.printf("%d		%d		%d%n", 8, 8*8, 8*8*8);
+		System.out.printf("%d		%d		%d%n", 9, 9*9, 9*9*9);
+		System.out.printf("%d		%d		%d%n", 10, 10*10, 10*10*10);
+
+
+	}
+
+}
